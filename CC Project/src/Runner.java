4b67b174cb5f1def453ee7a443d37c2e11c5448c
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2025-04-08 13:10:07 -0600

    initial commit

---
+++ CC Project/src/Runner.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.io.File;
+java.io.Exception;
+
+
+public class Runner
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Please put in a credit card number");
+				Scanner userIntInput = new Scanner(System.in);
+				System.out.println("hi");
+				//whats up
+//hiiiiii
+			}
+		
+		public static void readCreditCards() throws IOException
+		{
+			
+		}
+		
+		public static void doubleNumber()
+		{
+			
+		}
+		
+		public static void replaceNumber()
+		{
+			
+		}
+		
+		public static void check()
+		{
+			
+		}
+
+	}
