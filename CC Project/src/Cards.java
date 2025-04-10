3ebc0c30bf494529f39672fef38c9ce70871d76f
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2025-04-10 13:17:48 -0600

    Made some stuff idk if it works

---
+++ CC Project/src/Cards.java
@@ -0,0 +1,21 @@
+
+public class Cards
+	{
+		private int numbers;
+		
+		public Cards (int n)
+		{
+			numbers = n;
+		}
+		
+		public void setNumbers (int numbers)
+		{
+			this.numbers = numbers;
+		}
+		
+		public int getNumbers()
+		{
+			return numbers;
+		}
+
+	}
--- CC Project/src/Runner.java
+++ CC Project/src/Runner.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.io.File;
-java.io.Exception;
 
 
 public class Runner
@@ -10,13 +9,12 @@ public class Runner
 			{
 				System.out.println("Please put in a credit card number");
 				Scanner userIntInput = new Scanner(System.in);
-				System.out.println("hi");
-				//whats up
-//hiiiiii
+				int [] nums = userIntInput;
 			}
 		
-		public static void readCreditCards() throws IOException
+		public static void readCreditCards()
 		{
+			
 			
 		}
 		
